5e93ec9ca01149594cd6bcff4cdcf10660ebedc5
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2021-07-31 22:08:42 -0400

    Fix lint errors

--- modules/invite-protection.js
+++ modules/invite-protection.js
@@ -1,27 +1,33 @@
-var Database, dbClient, db, collection;
+let Database, dbClient, db, collection;
 
-module.exports = async function(bot, msg) {
+module.exports = async (bot, msg) => {
+
   if (!Database) {
+
     Database = await require("../database");
     dbClient = Database.mongoClient;
     db = dbClient.db("guilds");
     collection = db.collection("GuildInviteWhitelist");
   };
   
-  var matches = msg.content.match(/discord.gg\/(\S+)/mi);
+  const matches = msg.content.match(/discord.gg\/(\S+)/mi);
+  // eslint-disable-next-line arrow-body-style
   if (msg.channel.id !== "868879799601496084" && !msg.member.roles.find((r) => {
+
     return {
       "862071715441803285": r, 
       "862071540521369661": r,
       "549312685255294976": r,
       "753661816999116911": r
     }[r];
+
   }) && matches) {
     
     // Check if it's unverified
-    var verificationStatus = await collection.findOne({inviteCode: matches[1]});
+    const verificationStatus = await collection.findOne({inviteCode: matches[1]});
     
     if (!verificationStatus || verificationStatus.whitelisted !== 1) {
+
       await msg.delete();
       await msg.channel.createMessage({
         content: "<@" + msg.author.id + "> Please ask the <@&497608104230387713> before posting an unverified invite link.",
@@ -31,8 +37,11 @@ module.exports = async function(bot, msg) {
         }
       });
       return false;
-    };
-  };
+
+    }
+
+  }
   
   return true;
-}
+
+};
