a279339fdb74d0b97760c19acbf81831d840604f
Christian Toney <37517470+Sudobeast@users.noreply.github.com>
2021-08-10 22:29:22 -0400

    Fix invite approvals

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ bot.on("guildMemberAdd", async (guild, member) => {
 
     joinTimes.push(new Date().getTime());
     const currentAvg = joinTimes.reduce((a, b) => a + b, 0) / 5;
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 5; i++) {
 
       if (Number.isNaN(joinTimes[i])) break;
       suspicious = Math.abs(joinTimes[i] - currentAvg) < 30000;
@@ -75,11 +75,83 @@ async function loadDB() {
   dbClient = database.mongoClient;
   db = dbClient.db("guilds");
   collections = {
-    infractions: db.collection("Infractions")
+    infractions: db.collection("Infractions"),
+    inviteWhitelist: db.collection("GuildInviteWhitelist")
   };
 
   console.log("\x1b[32m%s\x1b[0m", "[Client] Database variables updated");
   
+}
+
+const checking = {};
+async function updateInviteVerification(verifyCodes, msg, emoji, reactor, deleted) {
+
+  // Debounce
+  reactor = reactor || (deleted && {id: -1});
+  checking[msg.id] = checking[msg.id] || {};
+  if (checking[msg.id] === reactor.id) return;
+  checking[msg.id][reactor.id] = 1;
+  
+  // Check if we're approving the invite now
+  msg = (msg.content && msg) || (!deleted && await bot.getMessage(msg.channel.id, msg.id));
+  const approvingNow = !deleted && verifyCodes;
+  let otherStaffApproved = !approvingNow && await msg.getReaction("✅");
+  otherStaffApproved = otherStaffApproved && otherStaffApproved.find(user => msg.channel.guild.members.find(member => member.id === user.id && member.roles.find(r => {
+
+    return {
+      "862071715441803285": r, 
+      "862071540521369661": r,
+      "549312685255294976": r,
+      "753661816999116911": r
+    }[r];
+
+  })));
+
+  // Check if the message is from the promo channel, has an invite link, was approved by a moderator, and was not approved before
+  reactor = (verifyCodes && reactor) || (!deleted && msg.channel.guild.members.find(member => member.id === reactor));
+  const inviteMatches = msg.content && [...msg.content.matchAll(/discord.gg\/(\S+)/gmi)];
+  if (msg.channel.id === "868879799601496084" && (deleted || (emoji.name === "✅" && inviteMatches[0] && reactor.roles.find((r) => {
+
+    return {
+      "862071715441803285": r, 
+      "862071540521369661": r,
+      "549312685255294976": r,
+      "753661816999116911": r
+    }[r];
+
+  })))) {
+
+    const updatedInvites = [];
+    for (let i = 0; inviteMatches.length > i; i++) {
+      
+      // Check if the invite hasn't been approved before
+      const code = inviteMatches[i][1];
+      const verificationStatus = await collections.inviteWhitelist.findOne((deleted && {messageId: msg.id}) || {inviteCode: code});
+      if ((verifyCodes && (!verificationStatus || verificationStatus.whitelisted !== 1)) || (!verifyCodes && (verificationStatus && verificationStatus.whitelisted === 1) && !otherStaffApproved)) {
+        
+        // Update the database3
+        await collections.inviteWhitelist.updateOne(
+          {inviteCode: code}, 
+          {$set: {whitelisted: (verifyCodes && 1) || 0, messageId: msg.id}},
+          {upsert: true}
+        );
+
+        // Success!
+        updatedInvites.push(code);
+        console.log("Approved invite " + code);
+      
+      }
+
+    }
+
+    // Tell the team
+    const plural = updatedInvites[1] ? "s" : "";
+    await bot.createMessage("543611772263989269", ((deleted && "A promo message with " + (plural ? "" : "an ") + "invite code" + (plural ? "s were" : " was") + " deleted, so I") || "<@" + reactor.id + ">") + " " + (!verifyCodes ? "un" : "") + "approved the following invite code" + plural + ": " + updatedInvites.join(", ") + ((deleted && ".") || ".\n\nMore info about the server here: https://discord.com/channels/" + msg.channel.guild.id + "/" + msg.channel.id + "/" + msg.id));
+
+  }
+
+  checking[msg.id][reactor.id] = undefined;
+
 }
 
 let startedLoading;
@@ -185,6 +257,12 @@ bot.on("ready", async () => {
     latestMessages[msg.author.id] = msg;
     
   });
+
+  bot.on("messageReactionAdd", async (msg, emoji, reactor) => await updateInviteVerification(true, msg, emoji, reactor));
+
+  bot.on("messageReactionRemove", async (msg, emoji, reactor) => await updateInviteVerification(false, msg, emoji, reactor));
+
+  bot.on("messageDelete", async msg => msg.channel.id === "868879799601496084" && await updateInviteVerification(false, msg, undefined, undefined, true));
   
   if (ExperimentalMode) {
 
